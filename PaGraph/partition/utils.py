5732c1d66ba5a891c0a954a4572ab5c66715bb58
Lin Zhang <56950883+lzhangbv@users.noreply.github.com>
2022-01-04 18:03:20 +0800

    Add comment for get_sub_graph in partition/utils

--- PaGraph/partition/utils.py
+++ PaGraph/partition/utils.py
@@ -7,7 +7,15 @@ import numpy as np
 import scipy.sparse as spsp
 
 def get_sub_graph(dgl_g, train_nid, num_hops):
+  """
+  get sub-graph sampled on graph `dgl_g` from seed nodes `train_nid` with `num_hops` hops
+  return:
+      csr_adj: csr adj representation in the subgraph space
+      sub2full: all sub-nodes in the full-graph space, as well as used to map sub-graph id into full-graph id
+      subtrainid: all train nodes in the subgraph space
+  """
   nfs = []
+  # neighbor sampling: return iterator of nodeflow
   for nf in dgl.contrib.sampling.NeighborSampler(dgl_g, len(train_nid),
                                                  dgl_g.number_of_nodes(),
                                                  neighbor_type='in',
@@ -18,10 +26,11 @@ def get_sub_graph(dgl_g, train_nid, num_hops):
                                                  prefetch=False):
     nfs.append(nf)
   
-  assert(len(nfs) == 1)
-  nf = nfs[0]
+  assert(len(nfs) == 1) # the number of nodeflow objects is calculated by len(seed_nodes) // batch_size
+  nf = nfs[0] # nodeflow
   full_edge_src = []
   full_edge_dst = []
+  # travel the nodeflow to store sub-nodes
   for i in range(nf.num_blocks):
     nf_src_nids, nf_dst_nids, _ = nf.block_edges(i, remap_local=False)
     full_edge_src.append(nf.map_to_parent_nid(nf_src_nids))
@@ -32,7 +41,7 @@ def get_sub_graph(dgl_g, train_nid, num_hops):
   sub2full = np.unique(np.concatenate((full_srcs, full_dsts)))
   full2sub = np.zeros(np.max(sub2full) + 1, dtype=np.int64)
   full2sub[sub2full] = np.arange(len(sub2full), dtype=np.int64)
-  # map to sub graph space
+  # create adj in sub graph space
   sub_srcs = full2sub[full_srcs]
   sub_dsts = full2sub[full_dsts]
   vnum = len(sub2full)
